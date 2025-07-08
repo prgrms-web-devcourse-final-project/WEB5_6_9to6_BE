b1b8ade627c69f80a1017b82ced26fb2ad2713ac
lnvisibledragon <roemflq1@gmail.com>
2025-07-08 18:04:36 +0900

    feat:아이템 관련 엔티티 추가 완료

---
+++ src/main/java/com/grepp/spring/app/model/reward/entity/OwnItem.java
@@ -0,0 +1,39 @@
+package com.grepp.spring.app.model.reward.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "own_item")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class OwnItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long OwnItemId;
+
+    @NotNull
+    private boolean IsUsed;
+
+    @NotNull
+    private boolean activated;
+
+    @ManyToOne
+    @JoinColumn(name = "reward_item")
+    private RewardItem rewardItem;
+
+}
