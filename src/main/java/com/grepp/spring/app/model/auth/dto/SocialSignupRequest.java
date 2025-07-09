4905da1c8a3f74ba16fa384d85b31092797d368c
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-07-09 18:14:13 +0900

    fix: 잘못된 검증 변경 NotBlank -> NotNull

---
+++ src/main/java/com/grepp/spring/app/model/auth/dto/SocialSignupRequest.java
@@ -0,0 +1,27 @@
+package com.grepp.spring.app.model.auth.dto;
+
+import com.grepp.spring.app.model.member.code.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor
+public class SocialSignupRequest {
+
+    @NotBlank(message = "닉네임이 비어있습니다.")
+    private String nickname;
+
+    @NotNull(message = "생일이 비어있습니다.")
+    private LocalDate birthday;
+
+    @NotNull(message = "성별이 비어있습니다.")
+    private Gender gender;
+
+    public SocialSignupRequest(String nickname, LocalDate birthday, Gender gender) {
+        this.nickname = nickname;
+        this.birthday = birthday;
+        this.gender = gender;
+    }
+}
