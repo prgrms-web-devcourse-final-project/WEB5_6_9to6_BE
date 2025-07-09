9a2b3d758f44a72df0189c54f45edd112b5989e3
lnvisibledragon <roemflq1@gmail.com>
2025-07-09 12:52:39 +0900

    feat:api 추가

--- src/main/java/com/grepp/spring/app/controller/api/RewardController.java
+++ src/main/java/com/grepp/spring/app/controller/api/RewardController.java
@@ -1,14 +1,20 @@
 package com.grepp.spring.app.controller.api;
 
 import com.grepp.spring.infra.response.CommonResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.hibernate.annotations.Fetch;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,4 +71,45 @@ public class RewardController {
             .status(HttpStatus.CREATED)
             .body(CommonResponse.success(data));
     }
+
+    @PatchMapping("/{itemId}/image")
+    @ApiResponse(responseCode = "200")
+    public ResponseEntity<Map<String, Object>> getItemImages(@PathVariable Long itemId) {
+        Map<String, Object> response = new LinkedHashMap<>();
+
+
+        // 짝수일 경우 서버에 이미지 존재
+        if (itemId% 2==0) {
+
+            response.put("code", "0000");
+            response.put("message", "이미지를 조회했습니다.");
+            Map<String, Object> data;
+            data = Map.of("wholeImageUrl", "https://example.com/images/whole-outfit-" + itemId + ".png");
+
+            response.put("data", data);
+            return ResponseEntity.ok(response);
+        } else { // 홀수 일 때 서버에 이미지 없음
+
+            response.put("code", "0001");
+            response.put("message", "서버에 이미지가 없습니다.");
+
+            return ResponseEntity.ok(response);
+        }
+    }
+
+    @PostMapping("/saveimage")
+    @ApiResponse(responseCode = "200")
+    public ResponseEntity<CommonResponse<Map<String, Object>>> PostItemImages(
+        @RequestBody(required = false) Map<String, Object> PostItemRequest) {
+        Map<String, Object> data = Map.of();
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(CommonResponse.success(data));
+    }
+
+
+
+
+
 }
