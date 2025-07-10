116ebdc488599548b7bf3880767dcb124ec028ad
lnvisibledragon <roemflq1@gmail.com>
2025-07-10 09:57:37 +0900

    chore:진짜진짜 진짜 쿼리 dsl관련 의존성 추가 완료

--- src/main/java/com/grepp/spring/infra/config/DBConfig.java
+++ src/main/java/com/grepp/spring/infra/config/DBConfig.java
@@ -1,4 +1,19 @@
 package com.grepp.spring.infra.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class DBConfig {
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
 }
