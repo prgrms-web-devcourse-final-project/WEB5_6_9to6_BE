c76c55b1eecd0c3f21f6ccc9795c173ae3a7c89a
youngjunHwang <ghkddudwns12345@naver.com>
2025-07-11 17:27:34 +0900

    Feat: 퀴즈 조회 구현, 엔티티 수정 (#49)
    * Feat:퀴즈 조회 구현
    
    * refactor: entity 패턴에맞게 수정

--- src/main/java/com/grepp/spring/app/model/quiz/entity/ChoiceEntity.java
+++ src/main/java/com/grepp/spring/app/model/quiz/entity/ChoiceEntity.java
@@ -1,40 +1,54 @@
 package com.grepp.spring.app.model.quiz.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
 @Entity
 @Table(name = "choice")
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChoiceEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long choiceId;
+    private Long choiceId;
 
-    @Column(nullable = false)
-    private long quizId;
-
+    @NotNull
     @Column(nullable = false)
     private String choice1;
 
+    @NotNull
     @Column(nullable = false)
     private String choice2;
 
+    @NotNull
     @Column(nullable = false)
     private String choice3;
 
+    @NotNull
     @Column(nullable = false)
     private String choice4;
 
+    @NotNull
     @Column(nullable = false)
     private boolean activated;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quizId")
+    private QuizEntity quiz;
+
+    @Builder
+    public ChoiceEntity(Long choiceId, String choice1, String choice2, String choice3, String choice4, boolean activated, QuizEntity quiz) {
+        this.choiceId = choiceId;
+        this.choice1 = choice1;
+        this.choice2 = choice2;
+        this.choice3 = choice3;
+        this.choice4 = choice4;
+        this.activated = activated;
+        this.quiz = quiz;
+    }
 }
--- src/main/java/com/grepp/spring/app/model/quiz/entity/QuizRecordEntity.java
+++ src/main/java/com/grepp/spring/app/model/quiz/entity/QuizRecordEntity.java
@@ -1,7 +1,8 @@
 package com.grepp.spring.app.model.quiz.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,24 +10,35 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "quiz_record")
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class QuizRecordEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long quizRecordId;
+    private Long quizRecordId;
 
+    @NotNull
     @Column(nullable = false)
-    private long studyMemberId;
+    private Long studyMemberId;
 
-    @Column(nullable = false)
-    private long quizSetId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quizSetId")
+    private QuizSetEntity quizSet;
 
+    @NotNull
     @Column(nullable = false)
     private boolean isPassed;
 
+    @NotNull
     @Column(nullable = false)
     private boolean activated;
+
+    @Builder
+    public QuizRecordEntity(Long quizRecordId, Long studyMemberId, QuizSetEntity quizSet, boolean isPassed, boolean activated) {
+        this.quizRecordId = quizRecordId;
+        this.studyMemberId = studyMemberId;
+        this.quizSet = quizSet;
+        this.isPassed = isPassed;
+        this.activated = activated;
+    }
 }
