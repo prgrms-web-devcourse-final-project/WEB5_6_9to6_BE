22be42472e720a12b1c81b81b9be9b0a594a872c
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-07-11 12:47:23 +0900

    feat: JWT Claims에서 memberId 추출하는 로직 메서드화

---
+++ src/main/java/com/grepp/spring/infra/util/SecurityUtil.java
@@ -0,0 +1,21 @@
+package com.grepp.spring.infra.util;
+
+import com.grepp.spring.app.model.auth.domain.Principal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Slf4j
+public class SecurityUtil {
+
+    private SecurityUtil() {}
+
+    public static Long getCurrentMemberId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !(authentication.getPrincipal() instanceof Principal)) {
+            throw new IllegalStateException("Security context에 인증 정보가 없습니다.");
+        }
+        Principal principal = (Principal) authentication.getPrincipal();
+        return principal.getMemberId();
+    }
+}
