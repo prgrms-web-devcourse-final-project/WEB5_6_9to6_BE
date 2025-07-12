27c009fb3903bab60c3d5fc1ee0442f9fa52acb3
inseon <dlsline0611@gmail.com>
2025-07-13 02:27:03 +0900

    feat: 알람, 알람 수신자 엔티티 생성

---
+++ src/main/java/com/grepp/spring/app/model/alarm/code/AlarmType.java
@@ -0,0 +1,7 @@
+package com.grepp.spring.app.model.alarm.code;
+
+public enum AlarmType {
+    NOTICE,
+    APPLY,
+    RESULT
+}
---
+++ src/main/java/com/grepp/spring/app/model/alarm/entity/Alarm.java
@@ -0,0 +1,50 @@
+package com.grepp.spring.app.model.alarm.entity;
+
+import com.grepp.spring.app.model.alarm.code.AlarmType;
+import com.grepp.spring.app.model.member.entity.Member;
+import com.grepp.spring.infra.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Alarm extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Member receiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Member sender;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    private AlarmType alarmType;
+
+    @OneToMany(mappedBy = "alarm", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AlarmRecipient> recipients = new ArrayList<>();
+
+}
