08c8e527bc97daa086210bfc1d9599b9a692e23e
inseon <dlsline0611@gmail.com>
2025-07-13 01:25:16 +0900

    feat: 스터디 멤버 목표 달성 확인 표시 로직 추가

--- src/main/java/com/grepp/spring/app/model/study/repository/GoalAchievementRepository.java
+++ src/main/java/com/grepp/spring/app/model/study/repository/GoalAchievementRepository.java
@@ -1,8 +1,14 @@
 package com.grepp.spring.app.model.study.repository;
 
+import com.grepp.spring.app.model.member.entity.StudyMember;
 import com.grepp.spring.app.model.study.entity.GoalAchievement;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GoalAchievementRepository extends JpaRepository<GoalAchievement, Long> {
+
+    List<GoalAchievement> findAllByStudyMemberAndIsAccomplishedTrue(StudyMember studyMember);
 
 }
