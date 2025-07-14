673706f1390445917e5fd4be6a8004047473024f
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-07-14 10:15:27 +0900

    feat: 스터디 검색 페이지네이션

---
+++ src/main/java/com/grepp/spring/infra/util/page/PageParam.java
@@ -0,0 +1,21 @@
+package com.grepp.spring.infra.util.page;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@Data
+public class PageParam {
+    @Min(value = 1, message = "페이지는 1보다 작을 수 없습니다.")
+    @Max(1000)
+    private int page = 1;
+    @Min(value = 1, message = "최소 1개는 가져와야합니다.")
+    @Max(1000)
+    private int size = 5;
+
+    public Pageable getPageable() {
+        return PageRequest.of(this.page - 1, this.size);
+    }
+}
