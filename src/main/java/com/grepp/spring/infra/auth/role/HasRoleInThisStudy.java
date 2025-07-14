3d96c43a40f4bf10d57571ac4fc61198ec67541e
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-07-14 23:39:23 +0900

    feat: 스터디 역할 확인 매개변수 복수 추가 및 이름변경

--- src/main/java/com/grepp/spring/infra/auth/role/HasRoleInThisStudy.java
+++ src/main/java/com/grepp/spring/infra/auth/role/HasRoleInThisStudy.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface OnlyThisStudyRole {
-    StudyRole value();
+public @interface HasRoleInThisStudy {
+    StudyRole[] value();
 }
--- src/main/java/com/grepp/spring/infra/auth/role/StudyRoleCheckAspect.java
+++ src/main/java/com/grepp/spring/infra/auth/role/StudyRoleCheckAspect.java
@@ -4,6 +4,7 @@ import com.grepp.spring.app.model.member.code.StudyRole;
 import com.grepp.spring.app.model.member.repository.StudyMemberRepository;
 import com.grepp.spring.infra.util.SecurityUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.JoinPoint;
@@ -23,29 +24,32 @@ public class StudyRoleCheckAspect {
 
     private final StudyMemberRepository studyMemberRepository;
 
-    @Before("@annotation(OnlyThisStudyRole)")
+    @Before("@annotation(com.grepp.spring.infra.auth.role.HasRoleInThisStudy)")
     public void checkRole(JoinPoint joinPoint) throws AccessDeniedException {
         Long memberId = SecurityUtil.getCurrentMemberId();
 
-        // HttpServletRequest 에서 studyId
+        // HttpServletRequest 에서 studyId 획득
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
         HttpServletRequest request = requestAttributes.getRequest();
 
-        // PathVariable로 넘어온 값들을 Map 형태로 가져옵니다.
+        // PathVariable로 넘어온 값 획득
         Map<String, String> pathVariables = (Map<String, String>) request.getAttribute(
             HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
         Long studyId = Long.parseLong(pathVariables.get("studyId"));
 
         // 허가된 역할
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        OnlyThisStudyRole checkStudyRole = signature.getMethod().getAnnotation(OnlyThisStudyRole.class);
-        StudyRole requiredRole = checkStudyRole.value();
+        HasRoleInThisStudy checkStudyRole = signature.getMethod().getAnnotation(HasRoleInThisStudy.class);
+        StudyRole[] requiredRoles = checkStudyRole.value();
 
         // 실제 역할
         StudyRole userRoleInStudy = studyMemberRepository.findRoleByStudyAndMember(studyId, memberId)
             .orElseThrow(() -> new AccessDeniedException("해당 스터디에 가입되지않았습니다."));
 
-        if (!userRoleInStudy.equals(requiredRole)) {
+        boolean isAuthorized = Arrays.stream(requiredRoles)
+            .anyMatch(role -> role.equals(userRoleInStudy));
+
+        if (!isAuthorized) {
             throw new AccessDeniedException("해당 역할은 접근 권한이 없습니다.");
         }
     }
