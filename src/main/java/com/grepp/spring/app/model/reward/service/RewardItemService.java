2094872ffeec4f1f27a00f675ecc5bb4fdc5f863
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-07-15 16:01:30 +0900

    Feat: 페이지 네이션 (#70)
    * feat: 커서 기반 스크롤 헬퍼
    
    * feat: 스터디 검색 페이지네이션
    
    * feat: 상점 검색 페이지네이션 및 예외처리
    
    * Style: 검증메시지 변경
    
    * refactor: 스터디 목록(검색) 검증 활성화
    
    * refactor: 리워드 아이템 조회 페이지 네이션 취소

--- src/main/java/com/grepp/spring/app/model/reward/service/RewardItemService.java
+++ src/main/java/com/grepp/spring/app/model/reward/service/RewardItemService.java
@@ -3,9 +3,14 @@ package com.grepp.spring.app.model.reward.service;
 import com.grepp.spring.app.model.reward.dto.RewardItemDto;
 import com.grepp.spring.app.model.reward.entity.RewardItem;
 import com.grepp.spring.app.model.reward.repository.RewardItemRepository;
+import com.grepp.spring.infra.error.exceptions.OutOfMinimumPageException;
+import com.grepp.spring.infra.error.exceptions.OutOfMinimumPageSizeException;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
@@ -23,6 +28,20 @@ public class RewardItemService {
             .map(RewardItemDto::fromEntity)
             .collect(Collectors.toList());
 
+    }
+
+    public Page<RewardItemDto> getItemList(Pageable pageable){
+        if (pageable.getPageNumber() < 1) throw new OutOfMinimumPageException("Page는 1보다 작을 수 없습니다.");
+        if (pageable.getPageSize() < 1)throw new OutOfMinimumPageSizeException("Size는 1보다 작을 수 없습니다.");
+
+        Pageable modifiedPageable = PageRequest.of(
+            pageable.getPageNumber() - 1,
+            pageable.getPageSize(),
+            pageable.getSort()
+        );
+
+        Page<RewardItem> itemsPage = rewardItemRepository.findAllByActivatedIsTrue(modifiedPageable);
+        return itemsPage.map(RewardItemDto::fromEntity);
     }
 
 }
--- src/main/java/com/grepp/spring/app/model/study/repository/StudyRepositoryCustom.java
+++ src/main/java/com/grepp/spring/app/model/study/repository/StudyRepositoryCustom.java
@@ -5,9 +5,13 @@ import com.grepp.spring.app.model.study.entity.Study;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StudyRepositoryCustom {
     List<Study> searchByFilterWithSchedules(StudySearchRequest request);
+
+    Page<Study> searchByFilterWithSchedules(StudySearchRequest req, Pageable pageable);
 
     Optional<Study> findByIdWithSchedulesAndGoals(Long studyId);
 }
