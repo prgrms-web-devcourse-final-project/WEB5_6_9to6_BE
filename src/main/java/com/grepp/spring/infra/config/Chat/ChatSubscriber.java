995a6eed2fe9d7a3227f893a2a30b2950ca1983d
최종우 <roemflq1@gmail.com>
2025-07-15 16:23:19 +0900

    Feat/chat 메세지큐 구현, 채팅방 예외처리 (#71)
    * style:빌더패턴, 커스텀 예외 관련 피드백 반영 했습니다.
    
    * feat/chat관련 entity만듦
    
    * feat:chat 중간 저장
    
    * feat:chat 채팅 및 채팅 조회 기능 구현 완료
    
    * feat:chat 현재 접속자 수 조회 구현 완료
    
    * Remove:채팅 프로토타입 html 삭제
    
    * feat:채팅  MQ 적용 완료
    
    * fix: 배포시 생겼던 문제 미리 방지
    
    * fix:채팅방에 잡상인 못들어오게 방어 완료
    
    * fix: worker가 메세지 켜지고 꺼져서 메세지가 밀리는 현상 수정
    
    * feat:채팅 2주 간격으로 삭제 하게 만들었습니다.
    
    * fix: 응답 객체 api형식으로 통일

---
+++ src/main/java/com/grepp/spring/infra/config/Chat/ChatSubscriber.java
@@ -0,0 +1,67 @@
+package com.grepp.spring.infra.config.Chat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.grepp.spring.app.controller.websocket.payload.ChatMessageRequest;
+import com.grepp.spring.app.controller.websocket.payload.ChatMessageResponse;
+import com.grepp.spring.app.model.auth.domain.Principal;
+import com.grepp.spring.app.model.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+
+// redis pub/sub 기반 구현
+@Component
+@RequiredArgsConstructor
+public class ChatSubscriber
+//    implements MessageListener
+{
+//
+//    private final ChatService chatService; // DB 저장용
+//    private final ObjectMapper objectMapper;
+//    private final SimpMessagingTemplate messagingTemplate;
+//
+//    @Override
+//    public void onMessage(Message message, byte[] pattern) {
+//        try {
+//            String body = new String(message.getBody());
+//
+//            ChatMessageRequest chat = objectMapper.readValue(body, ChatMessageRequest.class);
+//
+//
+//            // DB 저장
+//            // WebSocket 응답용 객체 생성
+//           ChatMessageResponse response=  chatService.saveChatMessage(chat.getStudyId(), chat, chat.getSenderId());
+//
+//
+//
+//            if (chat.getReceiverEmail() == null) {
+//                // 전체 채팅
+//                messagingTemplate.convertAndSend("/subscribe/" + chat.getStudyId(), response);
+//            } else {
+//                // 귓속말
+//                messagingTemplate.convertAndSendToUser(chat.getReceiverEmail(), "/queue/messages", response);
+//
+//
+//
+//
+//
+//                // 보낸 사람에게도 보내기(자기 메세지 확인용)
+//                if(!chat.getReceiverEmail().equals(chat.getSenderEmail())){
+//                    messagingTemplate.convertAndSendToUser(
+//                        chat.getSenderEmail(),
+//                        "/queue/messages",
+//                        response
+//                    );
+//                }
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+    }
+
