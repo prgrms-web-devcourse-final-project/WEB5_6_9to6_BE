02b78da9387201d85cc6a191a3812078ab5220cd
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-07-16 11:34:57 +0900

    refactor: MemberController Tag 작성

--- src/main/java/com/grepp/spring/app/controller/api/member/MemberController.java
+++ src/main/java/com/grepp/spring/app/controller/api/member/MemberController.java
@@ -14,6 +14,7 @@ import com.grepp.spring.app.model.reward.service.RewardItemService;
 import com.grepp.spring.infra.response.CommonResponse;
 import com.grepp.spring.infra.util.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.LinkedHashMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원 API", description = "회원 정보 조회 및 관리 관련 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/members")
