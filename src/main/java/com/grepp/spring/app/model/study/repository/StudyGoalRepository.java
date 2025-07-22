993f32eff2b864cb114bc098e3a39aa7453b61f5
youngjunHwang <ghkddudwns12345@naver.com>
2025-07-22 18:33:02 +0900

    Feat: quiz test code 작성 (#112)
    * refactor: 예외처리 리팩토링
    
    * refactor: 퀴즈예외 패키지 이동
    
    * docs: QuizController swagger 작성
    
    * feat: testcode추가
    
    * feat : Dockerfile 추가
    
    * Update application.properties
    
    * refactor: 피드백 반영

--- src/main/java/com/grepp/spring/app/model/study/repository/StudyGoalRepository.java
+++ src/main/java/com/grepp/spring/app/model/study/repository/StudyGoalRepository.java
@@ -4,7 +4,6 @@ import com.grepp.spring.app.model.study.entity.StudyGoal;
 import com.grepp.spring.app.model.study.reponse.GoalsResponse;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
