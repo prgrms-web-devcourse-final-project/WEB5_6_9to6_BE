36220a4350ef0562bd037f1ca28e4171fdf15312
최종우 <roemflq1@gmail.com>
2025-07-23 18:57:36 +0900

    Fix:reward item change (#120)
    * style:빌더패턴, 커스텀 예외 관련 피드백 반영 했습니다.
    
    * feat/chat관련 entity만듦
    
    * feat:chat 중간 저장
    
    * feat:chat 채팅 및 채팅 조회 기능 구현 완료
    
    * feat:chat 현재 접속자 수 조회 구현 완료
    
    * Remove:채팅 프로토타입 html 삭제
    
    * feat:채팅  MQ 적용 완료
    
    * fix: 배포시 생겼던 문제 미리 방지
    
    * fix:채팅방에 잡상인 못들어오게 방어 완료
    
    * fix: worker가 메세지 켜지고 꺼져서 메세지가 밀리는 현상 수정
    
    * feat:채팅 2주 간격으로 삭제 하게 만들었습니다.
    
    * fix: 응답 객체 api형식으로 통일
    
    * fix: 아이템 변경시  멤버테이블의  아바타 이미지가 수정되지 않던 현상을 수정했습니다.
    
    * fix: 리워드 관련 서버에러 해결!
    
    * docs: 스웨거 명세를 작성했습니다
    
    * fix:프로퍼티 되돌리기
    
    * fix:리워드 아이템 변경이 잘 안되던거 querydsl 기반으로 수정
    
    * fix: 추가 수정
    
    ---------
    
    Co-authored-by: Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>

--- src/main/java/com/grepp/spring/app/model/reward/repository/OwnItemRepositoryCustom.java
+++ src/main/java/com/grepp/spring/app/model/reward/repository/OwnItemRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.grepp.spring.app.model.reward.repository;
 
+import com.grepp.spring.app.model.reward.code.ItemType;
 import com.grepp.spring.app.model.reward.dto.OwnItemDto;
 import java.util.List;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface OwnItemRepositoryCustom {
     List<OwnItemDto> findOwnItemsByMemberId(Long memberId);
 
+    long bulkUnsetUsedItemsByType(Long memberId, ItemType itemType);
 }
