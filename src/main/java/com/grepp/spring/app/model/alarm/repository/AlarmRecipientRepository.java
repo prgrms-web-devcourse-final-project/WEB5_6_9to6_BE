d01a0baffa2c15bed0ebd636ef68aed33c8a8ea5
inseon <dlsline0611@gmail.com>
2025-07-25 07:30:13 +0900

    chore: import 정리

--- src/main/java/com/grepp/spring/app/model/alarm/repository/AlarmRecipientRepository.java
+++ src/main/java/com/grepp/spring/app/model/alarm/repository/AlarmRecipientRepository.java
@@ -1,11 +1,7 @@
 package com.grepp.spring.app.model.alarm.repository;
 
 import com.grepp.spring.app.model.alarm.entity.AlarmRecipient;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
