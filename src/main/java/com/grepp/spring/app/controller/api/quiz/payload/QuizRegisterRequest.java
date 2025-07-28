162c0b0af31427bea26962b0bf6fe27d62aaf5f9
youngjun <ghkddudwns12345@naver.com>
2025-07-28 11:20:54 +0900

    refactor: @NoArgsConstructor 어노테이션 추가

--- src/main/java/com/grepp/spring/app/controller/api/quiz/payload/QuizRegisterRequest.java
+++ src/main/java/com/grepp/spring/app/controller/api/quiz/payload/QuizRegisterRequest.java
@@ -2,9 +2,11 @@ package com.grepp.spring.app.controller.api.quiz.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
     public class QuizRegisterRequest {
     private int week;
 }
