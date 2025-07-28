5b951f8e86d6d0a3a4bd088c6de0e2637e6dc11d
youngjunHwang <ghkddudwns12345@naver.com>
2025-07-28 10:10:56 +0900

    Refactor: entity, dto, request, response 통일성 있게 리팩토링했습니다 (#133)

--- src/main/java/com/grepp/spring/app/controller/api/quiz/payload/QuizRegisterRequest.java
+++ src/main/java/com/grepp/spring/app/controller/api/quiz/payload/QuizRegisterRequest.java
@@ -2,10 +2,8 @@ package com.grepp.spring.app.controller.api.quiz.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
     public class QuizRegisterRequest {
     private int week;
