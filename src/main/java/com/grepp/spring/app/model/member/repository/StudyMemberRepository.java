7f4f30d3da732c4b3074fdb1c0fd7ca54f5b3510
inseon <dlsline0611@gmail.com>
2025-07-28 21:32:48 +0900

    feat: 스터디장 조회하는 메서드 추가

--- src/main/java/com/grepp/spring/app/model/member/repository/StudyMemberRepository.java
+++ src/main/java/com/grepp/spring/app/model/member/repository/StudyMemberRepository.java
@@ -1,6 +1,8 @@
 package com.grepp.spring.app.model.member.repository;
 
+import com.grepp.spring.app.model.member.code.StudyRole;
 import com.grepp.spring.app.model.member.entity.StudyMember;
+import com.grepp.spring.app.model.study.entity.Study;
 import com.grepp.spring.app.model.study.repository.StudyMemberRepositoryCustom;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,4 +18,6 @@ public interface StudyMemberRepository extends JpaRepository<StudyMember, Long>,
     boolean existsByMember_IdAndStudy_StudyIdAndActivatedTrue(Long memberId, Long studyId);
 
     Optional<StudyMember> findByStudy_StudyIdAndStudyMemberId(Long studyId, Long studyMemberId);
+
+    Optional<StudyMember> findByStudyAndStudyRole(Study study, StudyRole studyRole);
 }
