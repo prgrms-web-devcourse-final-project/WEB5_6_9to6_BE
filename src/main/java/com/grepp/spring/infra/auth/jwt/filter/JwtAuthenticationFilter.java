9e3185e77f928343996847102fcd4007a2443f89
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-07-31 00:53:36 +0900

    hotfix: 소셜로그인 인증객체 생성하도록 변경 (#170)

--- src/main/java/com/grepp/spring/infra/auth/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/grepp/spring/infra/auth/jwt/filter/JwtAuthenticationFilter.java
@@ -42,7 +42,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         List<String> excludePath = new ArrayList<>();
         excludePath.addAll(List.of("/favicon.ico", "/img", "/js","/css","/download"));
         excludePath.addAll(List.of("/error", "/api/member/exists", "/member/signin", "/member/signup"));
-        excludePath.addAll(List.of("/api/v1/auth/signup", "/api/v1/auth/login", "/api/v1/auth/reissue", "/api/v1/auth/email", "/api/v1/auth/oauth/first-regist"));
+        excludePath.addAll(List.of("/api/v1/auth/signup", "/api/v1/auth/login", "/api/v1/auth/reissue", "/api/v1/auth/email"));
         excludePath.addAll(List.of("/api/v1/studies/search", "/api/v1/studies/categories"));
         String path = request.getRequestURI();
         return excludePath.stream().anyMatch(path::startsWith);
