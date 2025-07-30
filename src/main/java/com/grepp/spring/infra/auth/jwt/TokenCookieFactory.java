8943d798902a5b02afbd68ffeaca1f024b12920f
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-07-30 17:31:07 +0900

    fix: cookieFactory 설정 변경

--- src/main/java/com/grepp/spring/infra/auth/jwt/TokenCookieFactory.java
+++ src/main/java/com/grepp/spring/infra/auth/jwt/TokenCookieFactory.java
@@ -5,21 +5,23 @@ import org.springframework.http.ResponseCookie;
 public class TokenCookieFactory {
     public static ResponseCookie create(String name, String value, Long expires) {
         return ResponseCookie.from(name, value)
-                   .maxAge(expires)
-                   .path("/")
-                   .httpOnly(false)             // HttpOnly
-                   .secure(true)
-                   .sameSite("None")// Secure
-                   .build();
+            .maxAge(expires)
+            .path("/")
+            .domain(".studium-9to6.vercel.app")
+            .httpOnly(true)             // HttpOnly
+            .secure(true)
+            .sameSite("None")// Secure
+            .build();
     }
-    
+
     public static ResponseCookie createExpiredToken(String name) {
         return ResponseCookie.from(name, "")
-                   .maxAge(0)
-                   .path("/")
-                   .httpOnly(false)             // HttpOnly
-                   .secure(true)
-                   .sameSite("None")// // Secure
-                   .build();
+            .maxAge(0)
+            .path("/")
+            .httpOnly(true)             // HttpOnly
+            .secure(true)
+            .sameSite("None")// // Secure
+            .build();
     }
 }
+
